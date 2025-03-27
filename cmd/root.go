9781ff3b3dadea82b56abce6db8a507e2ee0a753
rick <LinuxSuRen@users.noreply.github.com>
2025-03-27 09:08:58 +0000

    fix: the name should be iotdb instead of orm

--- cmd/root.go
+++ cmd/root.go
@@ -24,7 +24,7 @@ import (
 
 func NewRootCommand() (c *cobra.Command) {
 	opt := &option{
-		Extension: ext.NewExtension("orm", "store", 7071),
+		Extension: ext.NewExtension("iotdb", "store", 7071),
 	}
 	c = &cobra.Command{
 		Use:   opt.GetFullName(),
